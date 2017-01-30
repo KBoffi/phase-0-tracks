2f1afa75bba9e5d0fe718447cfb4c9d89b0619fa
Kristen Boffi <kboffi@gmail.com>
2017-01-30 07:11:22 -0800

    Create program to encrypt and decrypt passwords with Natalie

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,37 +1,48 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def encrypt(string)
   index = 0
+  newString = ""
   while index < string.length 
     letter = string[index]
     if letter == "z"
-      puts "a"
-    else 
-      puts letter.next
+      newString += "a"
+    else
+      newString += letter.next
     end
     index += 1
   end  
+  p newString
 end
-
-
+  
 
 def decrypt(string)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
+  newString = ""
   index = 0 
   while index < string.length 
   newIndex = alphabet.index(string[index]) - 1
-  puts alphabet[newIndex]
+  newString += alphabet[newIndex]
   index += 1
   end 
+  p newString
 end 
 
-#encrypt("abc")
-#encrypt("zed")
+#Driver Code
 
-#decrypt("bcd")
-#decrypt("afe")
+#asks the user the decrypt or encrypt preference 
+puts "Would you like to decrypt or encrypt your password?"
+en_or_de = gets.chomp
 
-decrypt(encrypt("swordfish"))
+#asks the user for their password 
+puts "What is your password?" 
+password = gets.chomp
 
-#It works because the order of evaluation causes 
-#swordfish to encrypt first, then decrypt.
+#printing their result
+if "#{en_or_de}" == "encrypt"
+  puts "Your new password is " + encrypt("#{password}")
+else 
+  puts "Your new password is " + decrypt("#{password}")
+end
+
+
+#It works because the order of evaluation causes  #swordfish to encrypt first, then decrypt.
