67a8623055a84d66daeb46701d77ef78ee4f9bd4
Kristen Boffi <kboffi@gmail.com>
2017-01-31 20:47:12 -0800

    Add all methods to method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -22,18 +22,21 @@
 " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+string = "The mystery of the missing first letter"
+num = string.length
+
+string.slice!(1, num)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".gsub("Elementary,    my   dear        Watson!", "Elementary, my dear Watson!")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".codepoints
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
