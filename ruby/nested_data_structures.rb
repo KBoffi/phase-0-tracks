0a068e19915231181a384b7686172f0120a992b3
Kristen Boffi <kboffi@gmail.com>
2017-02-04 11:35:44 -0800

    Add data nested data structure for contents of refrigerator and pantry in the kitchen

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,56 @@
+kitchen = {
+  refrigerator: {
+    crisper_bins: {
+      left_hand_bin: [
+        "kale", "onion", "red bell pepper"
+      ]
+      right_hand_bin: [
+        "limes", "apples", "pineapple"
+      ]
+    }
+    door_shelves: {
+      bottom_shelf: [
+        "tonic water", "sparkling water"
+      ]
+      middle_shelf: [
+        "capers", "pickles", "olives", "jam"
+      ]
+      top_shelf: [
+        "eggs", "butter"
+      ]
+    }
+    main_shelves: {
+      lower: [
+        "leftovers", "sausage", "fresh fish"
+      ]
+      middle: [
+        "beer", "white wine"
+      ]
+      upper: [
+        "cold brew", "coconut milk", "fresh fruit", "almond butter"
+      ]
+      }
+    }
+  }
+  pantry: {
+    shelf_1: {
+      canned_foods: [
+        "beans", "chicken stock", "sardines"
+      ]
+      grains: [
+        "pasta", "brown rice", "dried lentils"
+      ]
+      }
+    }
+    shelf_2: {
+      wine: [
+        "wine club wines", "gifted wines",
+      ]
+    }
+    shelf_3: {
+      spices: [
+        "salt", "paprkia", "cinnamon", "dry rub"
+      ]
+    }
+  }
+}
