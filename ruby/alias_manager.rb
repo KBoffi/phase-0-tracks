1844f96b33d429fbf9a77d6037d749284c619889
Kristen Boffi <kboffi@gmail.com>
2017-02-05 14:54:22 -0800

    Add working code that splits first and last name into separate items in a new array. Need to incorporate variables into .slice

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,4 @@
-# Pseudocode and write a method that takes a spy's real name (e.g., "Felicia Torres") 
-# and creates a fake name with it by doing the following:
-
-# Swapping the first and last name.
-# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', 
-# and all of the consonants (everything else besides the vowels) to the next consonant 
-# in the alphabet. 
-# (So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.)
-
-
-
+# PSEUDOCODE 
 # Reverse the order of the spy's name so last name is first and first name is last
 # Split up the name into individual letters
 # Make a list of vowels in this order: "aeiou"
@@ -22,29 +12,19 @@
 name = ["Felicia", "Torres"]
 name = name.reverse!
 name = name.join
-p name = name.split('')
-
-vowels = ["a", "e", "i", "o", "u"]
+name = name.split('')
 
 new_name = name.each {|letter| p letter.next!}
 
-p new_name.join
 
-# name[0] == "a"
+last_name = new_name.slice(0..5)
+first_name = new_name.slice(6..12)
 
-# name.each {|x| puts "This is the letter #{x}"}
+new_last = last_name.join
+new_first = first_name.join
 
+p new_array = [new_last, new_first]
 
-# vowels = ["a", "e", "i", "o", "u"]
-
-# def vowel_locator
-  
-# name = ["Felicia", "Torres"]
-# name = name.reverse!
-# name = name.join
-# p name = name.split('')
-
-# vowels = ["a", "e", "i", "o", "u"]
 
 
       
