baaab894f2edd4563c3484bc55713661075f85f9
Kristen Boffi <kboffi@gmail.com>
2017-02-07 18:55:19 -0800

    Add species, characteristics, and behavior info for puppies to puppy.rb

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,1 +1,17 @@
 # 6.1 Introduction to Classes
+
+# SPECIES ----------------------
+# Canis Lupis
+
+# CHARACTERISTICS --------------
+# Eye count: 2
+# Leg length: varies
+# Paw count: 4
+# Color: varies
+# Claws: yes
+
+# BEHAVIOR ---------------------
+# Run
+# Bark 
+# Chew 
+
