bff84dfcf9cad38ae394193f8d05b729d4becafd
Kristen Boffi <kboffi@gmail.com>
2017-02-10 23:25:44 -0800

    Refactor getter methods using attr_reader.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,5 @@
 class Santa
+  attr_reader :age, :ethnicity
 
   def speak
     puts "Ho, ho, ho! Haaappy Holidays!"
@@ -22,41 +23,35 @@ def celebrate_birthday(new_age)
 end
 
 def get_mad_at(reindeer_name)
- # @reindeer_ranking[-1] = "#{reindeer_name}"
- x = @reindeer_ranking.index("#{reindeer_name}")
- @reindeer_ranking.rotate!(x + 1)
+  x = @reindeer_ranking.index("#{reindeer_name}")
+  @reindeer_ranking.rotate!(x + 1)
   p @reindeer_ranking
 end
 
 def gender_change(new_gender)
   @gender = new_gender
+  puts "Santa is #{new_gender}."
 end
 
 #getter methods
-def age
-  @age
-end
+# def age
+#   @age
+# end
 
-def ethnicity
-  @ethnicity
-end
+# def ethnicity
+#   @ethnicity
+# end
 end
 
 
 #DRIVER CODE ======================================
 
 p nick = Santa.new("transgender male", "Chinese")
-
-p nick.celebrate_birthday(45)
-
+nick.celebrate_birthday(45)
 nick.get_mad_at("Cupid")
-
-p nick.gender_change("female")
-
+nick.gender_change("female")
 puts "Santa is #{nick.age}."
-
 puts "Santa is #{nick.ethnicity}."
-
 nick.speak
 nick.eat_milk_and_cookies("biscotti")
 
