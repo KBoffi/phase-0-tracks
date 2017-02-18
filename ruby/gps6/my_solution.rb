afdc21da696ed4b6570dd39b5bd2f23e35eddc40
Kristen Boffi <kboffi@gmail.com>
2017-02-18 10:05:36 -0800

    Refactor predicted_deaths and speed_of_spread methods.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -20,14 +20,14 @@ class VirusPredictor
 
 # Determining the impact of the virus by calling predicted_deaths and speed_of_spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
 # Predicting total number of deaths in state based on population density and population
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -46,21 +46,20 @@ class VirusPredictor
   end
 
 # Determining how many months it will take virus to spread depending on population density
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
