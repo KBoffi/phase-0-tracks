79adb30339ab2817aa9f3adeda7e2b3c2aa1a90c
Kristen Boffi <kboffi@gmail.com>
2017-03-02 22:23:15 -0800

    Add code to delete items from list.

--- databases/kitten_maker/groceries.rb
+++ databases/kitten_maker/groceries.rb
@@ -1,15 +1,10 @@
-# OPERATION KITTEN EXPLOSION!
+# GROCERY LIST
 
-# talk about Object Relational Mapping (ORM)
-
-# require gems
 require 'sqlite3'
 
-# create SQLite3 database
 db = SQLite3::Database.new("groceries.db")
 db.results_as_hash = true
 
-# learn about fancy string delimiters
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS groceries(
     id INTEGER PRIMARY KEY,
@@ -18,17 +13,9 @@ create_table_cmd = <<-SQL
   )
 SQL
 
-# create a kittens table (if it's not there already)
 db.execute(create_table_cmd)
 
-# add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
-
-# add LOOOOTS of kittens!
-# so. many. kittens. 
-#KittenExplosion
-
-puts "Add an item to the list."
+puts "Add an item to the list. Press 'done' when finished."
 item = gets.chomp 
 puts "Add a quantity for the item."
 quantity = gets.chomp.to_i
@@ -39,21 +26,22 @@ end
 
 add_item(db, "#{item}", "#{quantity}")
 
-puts "Enter purchased items to be removed from list."
+puts "Enter purchased items to be removed from list, or type 'skip'."
 purchased = gets.chomp 
 
 def remove_item(db, purchased)
   db.execute("DELETE FROM groceries WHERE item='#{purchased}'")
 end
 
-remove_item(db, "#{purchased}")
+if !purchased == "skip"
+  remove_item(db, "#{purchased}")
+else 
+  list = db.execute("SELECT * FROM groceries")
+  list.each do |purchase|
+  puts "#{purchase['item']}: #{purchase['quantity']}"
+  end
+end
 
-puts "Type 'list' to view the current grocery list."
-list = 
 
-# explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
+
 
