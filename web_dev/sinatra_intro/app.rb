b784d32485410bca0c2ddd29cd3deb68cfeb4d52
Kristen Boffi <kboffi@gmail.com>
2017-03-04 21:58:31 -0800

    Add contact and great job routes, as well as route to add to integers.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,34 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# A /contact route that displays an address (you can make up the address).
+
+get '/contact' do 
+  "222 E Hamlin St., Seattle WA 98102"
+end
+
+# A /great_job route that can take a person's name as a query parameter 
+# (not a route parameter) and say "Good job, [person's name]!". If the 
+# query parameter is not present, the route simply says "Good job!"
+
+get '/great_job' do 
+  name = params[:name]
+  if name 
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# A route that uses route parameters to add two numbers and respond 
+# with the result. The data types are tricky here -- when will the 
+# data need to be (or arrive as) a string?
+
+get '/:number_1/and/:number_2' do
+  z = params[:number_1].to_i + params[:number_2].to_i
+  "#{z}"
+end
+
+
+
